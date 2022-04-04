d85ebc93df392152a52ddaf42c5b2aac1f9eff58
张晨辉 <cheneyzhangchenhui@gmail.com>
2022-04-04 22:49:55 +0800

    修复bug

--- src/views/package/index.vue
+++ src/views/package/index.vue
@@ -378,7 +378,8 @@ export default {
       course[1].prop = course[1].prop + this.latestCourse
       course[0].label = course[0].label + this.latestCourse
       console.log('this.dialogForm', this.dialogForm)
-      this.dialogForm.splice(this.dialogForm.length - 2, 0, ...course)
+
+      this.dialogForm.splice(courses.length * 2 + 2, 0, ...course)
       console.log('this.dialogForm', this.dialogForm)
     },
     handleRemoveCourse(index) {
